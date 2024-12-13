24c60d119f66af3c82451ff3ad111490848c157c
RogueOneEcho <Um9ndWVPbmVFY2hv@pm.me>
2024-12-13 20:20:32 +0000

    fix: add error handling to prevent panic

--- src/server.rs
+++ src/server.rs
@@ -4,8 +4,9 @@ use axum::http::StatusCode;
 use axum::routing::get;
 use axum::Router;
 use gazelle_api::{GazelleClient, GazelleClientOptions};
+use log::warn;
 use rogue_config::{OptionsProvider, YamlOptionsProvider};
-use rogue_logging::Error;
+use rogue_logging::{Error, Logger};
 use tokio::net::TcpListener;
 
 pub struct Server {
@@ -36,24 +37,43 @@ impl Server {
     }
 }
 
-async fn metrics() -> Result<String, StatusCode> {
-    let options = get_options().map_err(|e| {
-        e.log();
-        StatusCode::from_u16(500).expect("status code should be valid")
-    })?;
+async fn metrics_internal() -> Result<String, Error> {
+    Logger::force_init("coda".to_owned());
+    let options = get_options()?;
     let mut output = String::new();
     for (client_options, user_options) in options {
         let mut api = GazelleClient::from_options(client_options.clone());
-        let user = api
-            .get_user(user_options.user)
-            .await
-            .expect("should be able to get user");
+        let user = match api.get_user(user_options.user).await {
+            Ok(user) => user,
+            Err(e) => {
+                warn!("Failed to get user for client: {}", client_options.name);
+                e.log();
+                continue;
+            }
+        };
         let user = User::from_gazelle(user);
-        let yaml = serde_yaml::to_string(&user).expect("should be able to serialize");
+        let yaml = match serde_yaml::to_string(&user) {
+            Ok(yaml) => yaml,
+            Err(e) => {
+                warn!(
+                    "Failed to deserialize user for client: {}",
+                    client_options.name
+                );
+                warn!("{e}");
+                continue;
+            }
+        };
         let metrics = yaml.replace(": ", &format!("{{name=\"{}\"}} ", client_options.name));
         output.push_str(&metrics);
     }
     Ok(output)
+}
+
+async fn metrics() -> Result<String, StatusCode> {
+    metrics_internal().await.map_err(|e| {
+        e.log();
+        StatusCode::from_u16(500).expect("status code should be valid")
+    })
 }
 
 fn get_options() -> Result<Vec<(GazelleClientOptions, UserOptions)>, Error> {
@@ -69,16 +89,17 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    pub async fn metrics_test() {
+    pub async fn metrics_test() -> Result<(), Error> {
         // Arrange
+        Logger::force_init("coda".to_owned());
         let clients: Vec<GazelleClientOptions> = YamlOptionsProvider::get().unwrap();
         let expected = clients.len() * 9;
 
         // Act
-        let result = metrics().await;
+        let result = metrics_internal().await?;
 
         // Assert
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap().lines().count(), expected);
+        assert_eq!(result.lines().count(), expected);
+        Ok(())
     }
 }
