82f12435c71cbd241747d0867066f12aec695be0
RogueOneEcho <Um9ndWVPbmVFY2hv@pm.me>
2024-12-31 14:44:22 +0000

    refactor: remove redundant built dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,8 @@
 pub use logging::*;
+use options::*;
 pub use server::*;
 use user::*;
-
-use options::*;
-mod options;
-
 mod logging;
+mod options;
 mod server;
 mod user;
-
-#[allow(clippy::needless_raw_strings)]
-mod built_info {
-    include!(concat!(env!("OUT_DIR"), "/built.rs"));
-}
